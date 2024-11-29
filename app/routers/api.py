80346568ceb631438bc9e1944f564706735f5552
kriskriskris0 <a7771977@gmail.com>
2024-11-29 18:21:59 +0500

    feat: 🤘🤘🤘🤘

--- app/routers/api.py
+++ app/routers/api.py
@@ -6,6 +6,6 @@ router = APIRouter()
 
 @router.post("/predict")
 def predict(input_data: TextInput):
-    result = predict_emotions(input_data)
+    result = predict_emotions(input_data.text)
     sorted_results = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
     return {"predicted_emotions": sorted_results}
